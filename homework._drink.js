dfda37babc926fc665cedc932ed10b8efe1a3595
Yashchenko Illia <illeeardi@gmail.com>
2023-10-05 00:41:08 +0300

    attemtp javascript

---
+++ homework._drink.js
@@ -0,0 +1,81 @@
+let age = prompt("How old are You for a drink?");
+
+// // Status person after 21 yo
+
+if (age > 21) {
+  alert("Victory! You are get ukrainian drink <spotyckach>!");
+  // } else {
+  //   console.log("You are get only whiskey!");
+}
+
+if (age > 20) {
+  alert("You are get whiskey!");
+  // } else {
+  //   console.log("don't get whiskey, yoy onlu get beer, or soda-drink, or toddy!");
+}
+
+// // Guys before 18 y.o.
+
+if (age > 17) {
+  alert("You are get beer!");
+  // } else {
+  //   console.log("You are get soda drink <coke>!");
+}
+
+// // Young person before 21 y.o.
+
+if (age > 13) {
+  alert("You are get soda drink <coke>!");
+} else {
+  alert("You are dont get soda-drink, you are only get sweet water <toddy>!");
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// age = "18";
+
+// age = "21";
+
+// age = "22";
+
+// Child before 14 y.o.
+
+// if (age > 21) {
+//   console.log("You are get whiskey!");
+// } else {
+//   console.log("don't get whiskey, yoy onlu get beer, or soda-drink, or toddy!");
+// }
+
+// // Guys before 18 y.o.
+
+// if (age > 18) {
+//   console.log("You are get beer!");
+// } else {
+//   console.log("You are get soda drink <coke>!");
+// }
+
+// // Young person before 21 y.o.
+
+// if (age > 14) {
+//   console.log("You are get soda drink <coke>!");
+// } else {
+//   console.log("You are dont get soda-drink, you are only get sweet water <toddy>!");
+// }
+
+// // Status person after 21 yo
+
+// if (age > 22) {
+//   console.log("Victory! You are get ukrainian drink >spotyckach>!");
+// } else {
+//   console.log("You are get only whiskey!");
+// }
+
+// console.log("Привіт, світ!");
+
+// if (умова1) {
+//   // виконується, якщо умова1 істинна
+// } else if (умова2) {
+//   // виконується, якщо умова2 істинна
+// } else {
+//   // виконується, якщо жодна з умов не виконується
+// }
